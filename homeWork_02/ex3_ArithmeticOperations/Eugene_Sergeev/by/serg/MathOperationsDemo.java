4a101d6dfbd0bd870a4835c6508732bbc3897a6a
SerGamEsNIUS <chemist-sergeev98@yandex.ru>
2020-02-25 16:08:39 +0300

    Added Hw2Task3

---
+++ homeWork_02/ex3_ArithmeticOperations/Eugene_Sergeev/by/serg/MathOperationsDemo.java
@@ -0,0 +1,70 @@
+package by.serg;
+
+public class MathOperationsDemo{
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the 'Math Operations Demo'!");
+        int a = 60;
+        int b = 30;
+
+        System.out.println("1) Addition '+':");
+        System.out.print("a = ");
+        System.out.print(a);
+        System.out.print(", b = ");
+        System.out.println(b);
+
+        int sum = a + b;
+
+        System.out.print("a + b = ");
+        System.out.print(a);
+        System.out.print(" + ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(sum);
+
+        System.out.println("2) Subtraction '-':");
+        System.out.print("a = ");
+        System.out.print(a);
+        System.out.print(", b = ");
+        System.out.println(b);
+
+        int diff = a - b;
+
+        System.out.print("a - b = ");
+        System.out.print(a);
+        System.out.print(" - ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(diff);
+
+        System.out.println("3) Multiplication '*':");
+        System.out.print("a = ");
+        System.out.print(a);
+        System.out.print(", b = ");
+        System.out.println(b);
+
+        int mult = a * b;
+
+        System.out.print("a * b = ");
+        System.out.print(a);
+        System.out.print(" * ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(mult);
+
+        System.out.println("4) Division '/':");
+        System.out.print("a = ");
+        System.out.print(a);
+        System.out.print(", b = ");
+        System.out.println(b);
+
+        int div = a / b;
+
+        System.out.print("a / b = ");
+        System.out.print(a);
+        System.out.print(" / ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.print(div);
+    }
+}
