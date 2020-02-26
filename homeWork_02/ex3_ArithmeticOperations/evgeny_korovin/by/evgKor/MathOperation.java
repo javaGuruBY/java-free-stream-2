aab8292cabeb061b0a4c82c8975516a2748410e0
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-02-26 17:31:56 +0300

    Please, check my Hw2 task3

---
+++ homeWork_02/ex3_ArithmeticOperations/evgeny_korovin/by/evgKor/MathOperation.java
@@ -0,0 +1,12 @@
+package by.evgKor;
+
+public class MathOperation {
+    public static void main(String[] args) {
+        int a=50;
+        int b=5;
+        System.out.println("сумма равна "+ (a+b));
+        System.out.println("разность равна "+ (a-b));
+        System.out.println("произведение равно "+(a*b));
+        System.out.println("частное равно "+(a/b));
+    }
+}
