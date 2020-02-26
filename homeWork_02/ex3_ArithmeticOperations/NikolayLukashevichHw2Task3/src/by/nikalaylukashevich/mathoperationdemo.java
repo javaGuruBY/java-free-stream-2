afcd9ea21378d75ca75bd47d07507f453a426168
NikolayLukashevich <61136934+NikolayLukashevich@users.noreply.github.com>
2020-02-26 23:59:38 +0200

    Adding my Hw2 Task 3

---
+++ homeWork_02/ex3_ArithmeticOperations/NikolayLukashevichHw2Task3/src/by/nikalaylukashevich/mathoperationdemo.java
@@ -0,0 +1,49 @@
+package by.nikalaylukashevich;
+
+public class mathoperationdemo {
+
+    public static void  main (string [] args ) {
+        System.out.println("Welcome to the 'Math Operations'!");
+        int a = 500;
+        int b = 100;
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int segmentation = a / b;
+
+        System.out.print("a = ");
+        System.out.print(a);
+        System.out.print(", ");
+        System.out.print("b = ");
+        System.out.print(b);
+
+        System.out.print("a + b = ");
+        System.out.print(a);
+        System.out.print(" + ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(addition);
+
+        System.out.print("a - b = ");
+        System.out.print(a);
+        System.out.print(" - ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(subtraction);
+
+        System.out.print("a * b = ");
+        System.out.print(a);
+        System.out.print(" * ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(multiplication);
+
+        System.out.print("a  b = ");
+        System.out.print(a);
+        System.out.print(" / ");
+        System.out.print(b);
+        System.out.print(" = ");
+        System.out.println(segmentation);
+    }
+
+}
