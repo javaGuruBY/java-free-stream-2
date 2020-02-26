5dd7351ed5c7ff08c98e818f67420c0771daf804
SergeyK92 <sergey.korshak.92@mail.ru>
2020-02-26 20:14:54 +0300

    My  task 3

---
+++ homeWork_02/ex3_ArithmeticOperations/SergeyKorshak/ArithmeticOperations/src/by/KorshakSergey/MathOperationsDemo.java
@@ -0,0 +1,25 @@
+package by.KorshakSergey;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 2;
+        int resultSum = a + b;
+        int resultDifference = a - b;
+        int resultMultiplication = a * b;
+        int resultDivision = a + b;
+        System.out.print("a = ");
+        System.out.println(a);
+        System.out.print("b = ");
+        System.out.println(b);
+        System.out.print("a + b = ");
+        System.out.println(resultSum);
+        System.out.print("a - b = ");
+        System.out.println(resultDifference);
+        System.out.print("a * b = ");
+        System.out.println(resultMultiplication);
+        System.out.print("a / b = ");
+        System.out.println(resultDivision);
+
+    }
+}
