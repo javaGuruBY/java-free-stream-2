4a67e825c64bf774b0780aed5ac2e040ec047cae
Soptimus <actanbl4@gmail.com>
2020-02-28 15:01:26 +0300

    add random generator

--- homeWork_02/ex3_ArithmeticOperations/AlesOstapchuk/src/by/ostapchuk/MathOperationsDemo.java
+++ homeWork_02/ex3_ArithmeticOperations/AlesOstapchuk/src/by/ostapchuk/MathOperationsDemo.java
@@ -6,7 +6,7 @@ public class MathOperationsDemo {
         double b = 8.0;
         System.out.println("a = " + a);
         System.out.println("b = " + b);
-        System.out.println("a + b = " + a + b);
+        System.out.println("a + b = " + (a + b));
         System.out.println("a - b = " + (a - b));
         System.out.println("a * b = " + (a*b));
         System.out.println("a / b = " + (a/b));
