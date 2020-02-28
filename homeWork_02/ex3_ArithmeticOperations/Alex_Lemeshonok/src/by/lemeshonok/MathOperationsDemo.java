bec9f3aba36b32075ea5a5049ffb0e766ef3a17f
Alex Lemesh <sashalemeshonok@mail.ru>
2020-02-28 21:31:23 +0300

    commit Hw2 Task3 Alex Lemeshonok

---
+++ homeWork_02/ex3_ArithmeticOperations/Alex_Lemeshonok/src/by/lemeshonok/MathOperationsDemo.java
@@ -0,0 +1,34 @@
+package by.lemeshonok;
+
+public class MathOperationsDemo {
+    public static void main (String[] args) {
+        System.out.println("Welcome, to the 'Math Operation'!");
+        System.out.println("'+'");
+        int a = 5;
+        int b = 10;
+        System.out.println("a =" + a + ", b =" + b);
+        int sum = a + b;
+        System.out.println("a + b =" + a + " + " + b + " = " + sum);
+
+        System.out.println("'-'");
+        int x = 200;
+        int y = 100;
+        System.out.println("x = " + x + ", y = " + y);
+        int difference = x - y;
+        System.out.println("x - y = " + x + " - " + y + " = " + difference);
+
+        System.out.println("'/'");
+        int q = 20;
+        int w = 20;
+        System.out.println("q = " + q + ", w = " + w);
+        int division = q / w;
+        System.out.println("q / w = " + q + " / " + w + " = " + 20 / 20);
+
+        System.out.println("'*'");
+        int e = 21;
+        int r = 10;
+        System.out.println("e =" + e + ", r =" + r);
+        System.out.println("e * r =" + e + " * " + r + " = " + 21 * 10);
+
+    }
+}
