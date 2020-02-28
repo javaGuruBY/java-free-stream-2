e9912d90baf8a2854d3985ab8c4b83e07ba8b289
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-02-28 14:59:24 +0300

    Update RandomGeneratorDemo.java

--- homeWork_02/ex4_RandomNumberGenerator/EvgenyKorovin/by/evgKor/RandomGeneratorDemo.java
+++ homeWork_02/ex4_RandomNumberGenerator/EvgenyKorovin/by/evgKor/RandomGeneratorDemo.java
@@ -10,7 +10,7 @@ public class RandomGeneratorDemo {
         System.out.println("num1= "+num1);
         System.out.println("num2= "+num2);
         System.out.println("num3= "+num3);
-        int result=num1*num2*num3;
+        int result=num1+num2+num3;
         System.out.println("calculation result= "+result);
 
 
