062eacf54cde231b5e7ea7590540d5879caef0ca
Korshak Sergey <61161255+SergeyK92@users.noreply.github.com>
2020-02-29 00:52:56 +0300

    Изменил названия первых двух переменных

--- homeWork_02/ex3_ArithmeticOperations/SergeyKorshak/ArithmeticOperations/src/by/KorshakSergey/MathOperationsDemo.java
+++ homeWork_02/ex3_ArithmeticOperations/SergeyKorshak/ArithmeticOperations/src/by/KorshakSergey/MathOperationsDemo.java
@@ -2,24 +2,24 @@ package by.KorshakSergey;
 
 public class MathOperationsDemo {
     public static void main(String[] args) {
-        int a = 8;
-        int b = 2;
-        int resultSum = a + b;
-        int resultDifference = a - b;
-        int resultMultiplication = a * b;
-        int resultDivision = a + b;
+        int firstInt = 8;
+        int secondInt = 2;
+        int resultSum = firstInt + secondInt;
+        int resultDifference = firstInt - secondInt;
+        int resultMultiplication = firstInt * secondInt;
+        int resultDivision = firstInt / secondInt;
         System.out.println("Пример математических вычислений тип переменных int");
-        System.out.print("a = ");
-        System.out.println(a);
-        System.out.print("b = ");
-        System.out.println(b);
-        System.out.print("a + b = ");
+        System.out.print("firstInt = ");
+        System.out.println(firstInt);
+        System.out.print("secondInt = ");
+        System.out.println(secondInt);
+        System.out.print("firstInt + secondInt = ");
         System.out.println(resultSum);
-        System.out.print("a - b = ");
+        System.out.print("firstInt - secondInt = ");
         System.out.println(resultDifference);
-        System.out.print("a * b = ");
+        System.out.print("firstInt * secondInt = ");
         System.out.println(resultMultiplication);
-        System.out.print("a / b = ");
+        System.out.print("firstInt / secondInt = ");
         System.out.println(resultDivision);
 
     }
