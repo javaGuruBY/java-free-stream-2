8be6b0bbf1a43be30806932df37a23628be47816
Alexhorosko <zega_boll@mail.ru>
2020-03-02 13:56:26 +0300

    My Hw2Task4 исправленный вриант

--- homeWork_02/ex4_RandomNumberGenerator/Aliaksandr_Horosko/src/by/aliaksandrhorosko/RandomGeneratorDemo.java
+++ homeWork_02/ex4_RandomNumberGenerator/Aliaksandr_Horosko/src/by/aliaksandrhorosko/RandomGeneratorDemo.java
@@ -6,6 +6,9 @@ public class RandomGeneratorDemo {
         double b = Math.random() * 10;
         double c = Math.random() * 1;
         double result = a + b + c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         System.out.println(result);
     }
 }
