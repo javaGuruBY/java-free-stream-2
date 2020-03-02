55c40bdead39f133db1c819831c4486b9b60ce46
Alexhorosko <zega_boll@mail.ru>
2020-03-02 12:50:12 +0300

    My Hw2Task3

---
+++ homeWork_02/ex3_ArithmeticOperations/Aliaksandr_Horosko/src/by/aliaksandrhorosko/MathOperationsDemo.java
@@ -0,0 +1,16 @@
+package by.aliaksandrhorosko;
+
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 10;
+        int result = a + b;
+        int result1 = b - a;
+        int result2 = b / a;
+        int result3 = a * b;
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+}
