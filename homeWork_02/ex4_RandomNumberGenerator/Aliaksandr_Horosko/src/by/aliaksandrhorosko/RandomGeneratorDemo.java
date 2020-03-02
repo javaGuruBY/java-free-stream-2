328dd67f4906ea6eca60d696c4dd74fe54c90c7e
Alexhorosko <zega_boll@mail.ru>
2020-03-02 13:38:38 +0300

    My Hw2Task4

---
+++ homeWork_02/ex4_RandomNumberGenerator/Aliaksandr_Horosko/src/by/aliaksandrhorosko/RandomGeneratorDemo.java
@@ -0,0 +1,11 @@
+package by.aliaksandrhorosko;
+
+public class RandomGeneratorDemo {
+    public static void main(String[] args) {
+        double a = Math.random() * 5;
+        double b = Math.random() * 10;
+        double c = Math.random() * 1;
+        double result = a + b + c;
+        System.out.println(result);
+    }
+}
