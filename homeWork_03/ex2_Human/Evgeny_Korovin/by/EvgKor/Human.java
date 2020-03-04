b8d61f5d352605885099449d18d2725d94d4ed04
Evgeny Korovin <61135777+JekaKor77@users.noreply.github.com>
2020-03-04 23:59:13 +0300

    Please, check my homework3 task2

---
+++ homeWork_03/ex2_Human/Evgeny_Korovin/by/EvgKor/Human.java
@@ -0,0 +1,15 @@
+package by.EvgKor;
+
+public class Human {
+    public String name;
+    public int age;
+    public Human(String name,int age){
+        this.name=name;
+        this.age=age;
+
+    }
+    public void greet(){
+        System.out.println("Hello, my name is "+name+", I'm "+age+" years old");
+    }
+
+}
---
+++ homeWork_03/ex2_Human/Evgeny_Korovin/by/EvgKor/HumanDemo.java
@@ -0,0 +1,11 @@
+package by.EvgKor;
+
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human humanJeka=new Human("Jeka",22);
+        humanJeka.greet();
+        Human humanKatya=new Human("Katya",20);
+        humanKatya.greet();
+        }
+    }
+
