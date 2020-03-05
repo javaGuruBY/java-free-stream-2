8fce0aca9a563eb750d4732e62ae7e30c4a4371b
NikolayLukashevich <61136934+NikolayLukashevich@users.noreply.github.com>
2020-03-05 23:12:16 +0200

    Do my HW 3 task 2

---
+++ homeWork_03/ex2_Human/NikolayLukashevichHW3Task2/src/by/nlukash/Human.java
@@ -0,0 +1,16 @@
+package by.nlukash;
+
+public class Human {
+    public String name;
+    public int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void greet() {
+        System.out.println("Hi! My name is " + name + ", I'm " + age + " years old");
+
+    }
+}
+
