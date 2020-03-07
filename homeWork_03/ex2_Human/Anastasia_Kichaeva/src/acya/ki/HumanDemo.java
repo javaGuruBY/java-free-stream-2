053119f48ed2d9c8537e3154be2d115453dae1a4
Анастасия <anastasia.kichaeva.22@gmail.com>
2020-03-07 05:01:08 +0300

    Adding my Humans please

---
+++ homeWork_03/ex2_Human/Anastasia_Kichaeva/src/acya/ki/HumanDemo.java
@@ -0,0 +1,10 @@
+package acya.ki;
+
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human firstHuman = new Human("Vova", 33);
+        firstHuman.greet();
+        Human secondHuman = new Human("Olya", 24);
+        secondHuman.greet();
+    }
+}
