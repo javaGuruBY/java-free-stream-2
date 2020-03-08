1b006ff8176247ebc78a91a991ffb3275255a567
Alexhorosko <zega_boll@mail.ru>
2020-03-08 23:23:00 +0300

    My Hw3Task4

---
+++ homeWork_03/ex4_Encoder/Aliaksandr_Horosko/src/by/alhr/Encoder.java
@@ -0,0 +1,11 @@
+package by.alhr;
+
+public class Encoder {
+    public static void encode(short code) {
+        System.out.println((char) code);
+    }
+
+    public static void decode(char symvol) {
+        System.out.println((short) symvol);
+    }
+}
---
+++ homeWork_03/ex4_Encoder/Aliaksandr_Horosko/src/by/alhr/EncoderDemo.java
@@ -0,0 +1,10 @@
+package by.alhr;
+
+public class EncoderDemo {
+    public static void main(String[] args) {
+        short Short = 100;
+        char Char = 'C';
+        Encoder.encode(Short);
+        Encoder.decode(Char);
+    }
+}
