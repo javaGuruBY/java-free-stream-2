44f6004d4b4e965c873c66be59906cdd833abd95
Alexhorosko <zega_boll@mail.ru>
2020-03-21 13:39:30 +0300

    ufiuxfdxkhsixudvhs.v

--- homeWork_03/ex4_Encoder/Aliaksandr_Horosko/src/by/alhr/EncoderDemo.java
+++ homeWork_03/ex4_Encoder/Aliaksandr_Horosko/src/by/alhr/EncoderDemo.java
@@ -2,7 +2,7 @@ package by.alhr;
 
 public class EncoderDemo {
     public static void main(String[] args) {
-        short Short = 100;
+        short Short = 200;
         char Char = 'C';
         Encoder.encode(Short);
         Encoder.decode(Char);
